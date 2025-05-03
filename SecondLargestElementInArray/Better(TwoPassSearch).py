925cb05118534f42e4cef77f192b14036f1a5bee
Nana Bentum Ansah <nanabentumansah@gmail.com>
2025-05-03 18:58:28 +0100

    Finding the second largest element in array

---
+++ SecondLargestElementInArray/Better(TwoPassSearch).py
@@ -0,0 +1,44 @@
+# The approach is to traverse the array twice.
+# In the first traversal, find the maximum element.
+# In the second traversal, find the maximum element ignoring
+# the one we found in the first traversal
+
+
+## Python program to find the second largest element in an array
+
+
+
+def getSecondLargest(arr):
+    n = len(arr)
+    
+    largest = -1
+    secondLargest = -1
+    
+    #first traverse for the largest element in the array
+    for i in range(n):
+        if arr[i] > largest:
+            largest = arr[i]
+        
+    # Finding the second largest element in the array
+    for i in range(n):
+        # update second largest if the current element is greater than 
+        # the second largest element and less than the largest element
+        if secondLargest < arr[i] < largest:
+            secondLargest = arr[i]
+        
+    return secondLargest
+    
+    
+if __name__ == "__main__":
+    arr = [10, 10, 10]
+    # arr = [12, 35, 1, 10, 34, 1]
+    # arr = [10, 5, 10]
+    print(getSecondLargest(arr))
+    
+ 
+    
+# Time Complexixty: O(2*n) = O(n),
+# as we are traversing the array two times.
+
+# Auxillary space: O(1),
+# as no extra space is required.
---
+++ SecondLargestElementInArray/Naive(Sorting).py
@@ -0,0 +1,43 @@
+# # The idea is to sort the array in non-descending order.
+# Now, we know the largest element will be at index (n - 1).
+# So, starting from index (n - 2), traverse the remaining array in reverse order.
+# As soon as we encounter an element which is not equal to the largest element,
+# return it as the second largest element in the array.
+# If all the elements are equal to the largest element, return -1.
+
+
+#Python program to find the second largest element in an array using sorting
+secondLargest = 0
+
+
+def getSecondLargest(arr):
+    n = len(arr)
+    
+    # Sort the array in non-decreasing order
+    arr.sort()
+    
+    # Start from the second last element, as last element is the largest element
+    for i in range(n-2, -1, -1):
+        
+        # return the first element which is not equal to the largest element
+        if arr[i] != arr[n-1]:
+            secondLargest = arr[i]
+            return secondLargest
+        
+        # if no second largest element was found, return -1
+        return -1
+    
+    
+if __name__ == "__main__":
+    arr = [12, 35, 1, 10, 34, 1]
+    # arr = [10, 5, 10]
+    # arr = [10, 10, 10]
+    print(getSecondLargest(arr))
+    
+    
+    
+## Time Complexity: O(n*log(n)),
+# as sorting the arrat takes O(n*log(n)) time
+# and traversing the arrat can take O(n) time in the worst case
+# so total time complexity = (n*log(n) + n) = O(n*log(n))
+# Auxillary space: O(1), as no extra space is required.
